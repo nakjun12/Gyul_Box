3620b176d26a1676d44e1488b2d0169b5215f363
Gyuddi <samll8539@naver.com>
2023-04-11 13:51:37 +0900

    feat - #2 : Si, Town, Houseinfo, Review, Coordinate 엔티티 생성

---
+++ oneroom/src/main/java/jeju/oneroom/coordinate/entity/Coordinate.java
@@ -0,0 +1,20 @@
+package jeju.oneroom.coordinate.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Coordinate {
+    private double latitude;
+    private double longitude;
+
+    public Coordinate(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
---
+++ oneroom/src/main/java/jeju/oneroom/review/entity/Review.java
@@ -0,0 +1,44 @@
+package jeju.oneroom.review.entity;
+
+import jeju.oneroom.houseInfo.entity.HouseInfo;
+import jeju.oneroom.town.entity.Town;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+    @Id
+    @Column(name = "review_id")
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String advantage;
+    private String disadvantage;
+    private String adminCost;
+    private int interiorRate;
+    private int buildingRate;
+    private int trafficRate;
+    private int securityRate;
+    private int locationRate;
+    private String residenceYear;
+    private String floor;
+    private int avgRate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "houseInfo_id")
+    private HouseInfo houseInfo;
+}
