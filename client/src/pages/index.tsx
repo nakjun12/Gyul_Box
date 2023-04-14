f7586a04bcb11bbc49d803dde4d50a4efbce8260
nakjun <wns450@gmail.com>
2023-04-14 14:33:50 +0900

    [FE] fix : Modal 위치 수정

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,7 +8,6 @@ import TextInput from "@/component/atoms/textInput/TextInput";
 import WhiteButton from "@/component/atoms/whiteButton/WhiteButton";
 import YellowStar from "@/component/atoms/yellowStar/YellowStart";
 import Header from "@/component/organisms/header/Header";
-import Modal from "@/component/templates/modal/Modal";
 import Gyul from "../component/atoms/lottie/Gyul";
 import styles from "../styles/Home.module.scss";
 
@@ -19,8 +18,7 @@ export default function main({}: Props) {
     <>
       <Header />
       <div className={styles.btn}>main</div>
-      <Modal isOpen={true}>하이</Modal>
-      그치
+
       <Button />
       <Category />
       <Map />
