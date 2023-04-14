185cf7ff92bd709fcf4f8fca92b867bdbedc2c29
codemodel6@gmail.com <codemodel6@gmail.com>
2023-04-14 21:57:20 +0900

    [FE] feat - #26 : 헤더 작업 중 중간 커밋

--- client/src/component/organisms/Header/header.tsx
+++ client/src/component/organisms/Header/header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "../Header/Header.module.scss";
+import logoGyul from "../../icon/gyul_main.png";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.logoWrapper}>dd</div>
+      <div className={styles.logoWrapper}>
+        <img src={logoGyul} alt="귤 로고"></img>귤박스
+      </div>
       <div className={styles.ulWrapper}>
         <ul className={styles.ul}>
           <li className={styles.li}>1</li>
