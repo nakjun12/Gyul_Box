58c83444628258a7fd73c5846e94bf475193964f
codemodel6@gmail.com <codemodel6@gmail.com>
2023-04-14 13:59:50 +0900

    [FE] feat - #26 : 헤더 초기 작업

--- client/src/component/organisms/Header/header.tsx
+++ client/src/component/organisms/Header/header.tsx
@@ -4,5 +4,18 @@ import styles from "../Header/Header.module.scss";
 type Props = {};
 
 export default function Header({}: Props) {
-  return <div className={styles.wrapper}>Header</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.logoWrapper}>dd</div>
+      <div className={styles.ulWrapper}>
+        <ul className={styles.ul}>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </div>
+      <div className={styles.loginWrapper}>dd</div>
+    </div>
+  );
 }
