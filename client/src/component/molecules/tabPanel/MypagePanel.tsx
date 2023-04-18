fae9248d6a75cdcf1164d1e98548e09ae69a969f
nakjun12 <wns450@gmail.com>
2023-04-18 16:15:01 +0900

    [FE] feat - #44 : userBox 추가, MypagePanel 추가

--- client/src/component/molecules/tabPanel/MypagePanel.tsx
+++ client/src/component/molecules/tabPanel/MypagePanel.tsx
@@ -1,14 +1,15 @@
 //my page
-import Box from "@/component/atoms/box/box";
+import UserBox from "@/component/atoms/box/UserBox";
 import Interest from "@/component/atoms/lnterest/Interest";
-import styles from "./tabPanel.module.scss";
+import styles from "./TabPanel.module.scss";
 const MyPagePanel = () => {
   return (
     <section className={styles.tab_panel}>
-      김치
-      <Interest text="내 정보" />
-      <Box />
-      <Interest text="내 쪽지" on={true} />
+      <label>회원정보</label>
+      <UserBox />
+      <label>회원정보</label>
+      <Interest text="쪽지함" on={true} />
+      <label>회원정보</label>
       <Interest text="관심지역" on={true} />
     </section>
   );
