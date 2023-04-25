98f020b0796f55a0e7e3b6989d3efb9f8ccce7f8
nakjun12 <wns450@gmail.com>
2023-04-25 18:26:46 +0900

    [FE] Rename #78 : 배포를 위한 Rename

--- client/src/component/atoms/list/List.tsx
+++ client/src/component/atoms/list/List.tsx
@@ -4,6 +4,6 @@ type Props = {
   text?: string;
 };
 
-export default function Category({ text }: Props) {
+export default function List({ text }: Props) {
   return <button className={styles.category}>{text}</button>;
 }
