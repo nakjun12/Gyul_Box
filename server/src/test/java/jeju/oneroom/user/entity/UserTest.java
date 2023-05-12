f8f357eccc0dc058af72eb2768c61d7610c4219a
bangjaeyoung <qkdwo4613@gmail.com>
2023-05-12 13:33:57 +0900

    [BE] feat - #68 : WebSocket 라이브러리 추가 및 Message 도메인 삭제

--- server/src/test/java/jeju/oneroom/user/entity/UserTest.java
+++ server/src/test/java/jeju/oneroom/user/entity/UserTest.java
@@ -2,16 +2,12 @@ package jeju.oneroom.user.entity;
 
 import jeju.oneroom.area.entity.Area;
 import jeju.oneroom.common.entity.Coordinate;
-import jeju.oneroom.message.entity.Message;
 import jeju.oneroom.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -28,7 +24,7 @@ class UserTest {
 
 
         //given
-        User user = getUser(getArea(getCoordinate()), getMessage());
+        User user = getUser(getArea(getCoordinate()));
 
         //then
         User savedUser = userRepository.save(user); // db에 저장
@@ -36,18 +32,14 @@ class UserTest {
 
         assertEquals(user.getNickname(), findUser.getNickname()); // 직접 만든 User와 db에 저장 후 찾아온 것 비교
         assertEquals(user.getArea().getAreaName(), findUser.getArea().getAreaName());
-        assertEquals(user.getSends().size(), findUser.getSends().size());
 
     }
 
-    private User getUser(Area area, Message message) {
-        List<Message> sends = new ArrayList<>();
-        sends.add(message);
+    private User getUser(Area area) {
         User user = User.builder()
                 .nickname("망나니DD 개발자")
                 .email("aaSDDa@naver.com")
                 .area(area)
-                .sends(sends)
                 .build();
         return user;
     }
@@ -64,12 +56,5 @@ class UserTest {
     private Coordinate getCoordinate() {
         Coordinate coordinate = new Coordinate(11.11111, 11.11111);
         return coordinate;
-    }
-
-    private Message getMessage() {
-        return Message.builder()
-                .content("양도때매 쪽지 드립니다.")
-                .readed(Boolean.FALSE)
-                .build();
     }
 }
