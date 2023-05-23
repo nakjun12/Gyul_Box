482fdacf311548efc569808ce88a59322526282a
nakjun12 <wns450@gmail.com>
2023-05-23 23:58:58 +0900

    [FE] Feat #100 : API 연결하기

--- client/src/pages/review/editor/index.tsx
+++ client/src/pages/review/editor/index.tsx
@@ -4,11 +4,30 @@ import LoginButton from "../../../component/atoms/loginButton/LoginButton";
 import Buliding from "../../../component/organisms/editorInformation/Buliding";
 import GoodBad from "../../../component/organisms/editorInformation/GoodBad";
 import Satisfactions from "../../../component/organisms/editorInformation/Satisfaction";
+import type { Editor_type, Rate } from "../../../utils/types/types";
+import { review_create } from "../../api/editor";
 type Props = {};
 
 export default function Index({}: Props) {
   const [onandoff, setOnandoff] = useState<boolean[]>([false, false, false]);
-
+  const [editorData, setEditorData] = useState<Editor_type>({
+    buildingName: "",
+    advantage: "",
+    disadvantage: "",
+    adminCost: "",
+    residenceYear: "2018-2023",
+    floor: "고층",
+    buildingType: "빌라",
+    rate: {
+      interiorRate: 0,
+      buildingRate: 0,
+      trafficRate: 0,
+      securityRate: 0,
+      locationRate: 0,
+      avgRate: null,
+    },
+    address: "",
+  });
   const handler = (index: number) => {
     setOnandoff((prev) => {
       return prev.map((_, i) => {
@@ -19,6 +38,67 @@ export default function Index({}: Props) {
       });
     });
   };
+  const postHandler = () => {
+    // 필수 입력값 체크
+    if (!editorData.buildingName) {
+      console.log("buildingName이 비어있습니다.");
+      return;
+    }
+    if (!editorData.advantage) {
+      console.log("advantage가 비어있습니다.");
+      return;
+    }
+    if (!editorData.disadvantage) {
+      console.log("disadvantage가 비어있습니다.");
+      return;
+    }
+    if (!editorData.adminCost) {
+      console.log("adminCost가 비어있습니다.");
+      return;
+    }
+    if (!editorData.residenceYear) {
+      console.log("residenceYear가 비어있습니다.");
+      return;
+    }
+    if (!editorData.floor) {
+      console.log("floor가 비어있습니다.");
+      return;
+    }
+    if (!editorData.buildingType) {
+      console.log("buildingType이 비어있습니다.");
+      return;
+    }
+    if (!editorData.address) {
+      console.log("address가 비어있습니다.");
+      return;
+    }
+    if (!isValidRate(editorData.rate)) {
+      console.log("rate가 유효하지 않습니다.");
+      return;
+    }
+
+    // 필수 입력값이 모두 채워져 있고 rate가 유효할 경우에 대한 처리
+    console.log("게시물을 등록합니다.");
+    review_create(editorData);
+    // 등록 로직 작성
+  };
+
+  const isValidRate = (rate: Rate) => {
+    // 각 속성의 값이 유효한지 확인
+    if (
+      rate.interiorRate === 0 ||
+      rate.buildingRate === 0 ||
+      rate.trafficRate === 0 ||
+      rate.securityRate === 0 ||
+      rate.locationRate === 0
+    ) {
+      return false;
+    }
+
+    return true;
+  };
+
+  console.log(editorData);
   return (
     <main className="review_editor_container">
       <div className="editor_wrapper">
@@ -43,7 +123,7 @@ export default function Index({}: Props) {
             transition: "all 1s ease-in-out",
           }}
         >
-          <Buliding />
+          <Buliding setEditorData={setEditorData} />
         </div>
       </div>
       <div className="editor_wrapper">
@@ -68,7 +148,7 @@ export default function Index({}: Props) {
             transition: "all 1s ease-in-out",
           }}
         >
-          <Satisfactions />
+          <Satisfactions setEditorData={setEditorData} />
         </div>
       </div>
       <div className="editor_wrapper">
@@ -93,7 +173,7 @@ export default function Index({}: Props) {
             transition: "all 1.2s ease-in-out",
           }}
         >
-          <GoodBad />
+          <GoodBad setEditorData={setEditorData} />
         </div>
       </div>
       <div
@@ -101,7 +181,7 @@ export default function Index({}: Props) {
         style={{ marginTop: "30px", height: "70px" }}
       >
         <div style={{ marginTop: "30px", height: "70px" }}>
-          <LoginButton>작성하기</LoginButton>
+          <LoginButton onClick={postHandler}>작성하기</LoginButton>
         </div>
       </div>
     </main>
